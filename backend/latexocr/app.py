0a9308cc15c780e2981bdc281665f00643f29422
Xiangzhuang Shen <datahonor@gmail.com>
2024-01-12 15:59:47 +0800

    test

---
+++ backend/latexocr/app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import gradio as gr
+
+CUSTOM_PATH = "/gradio"
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_main():
+    return {"message": "This is your main app"}
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello World"}
+
+
+io = gr.Interface(lambda x: "Hello, " + x + "!", "textbox", "textbox")
+app = gr.mount_gradio_app(app, io, path=CUSTOM_PATH)
+
+
+# Run this from the terminal as you would normally start a FastAPI app: `uvicorn run:app`
+# and navigate to http://localhost:8000/gradio in your browser.
